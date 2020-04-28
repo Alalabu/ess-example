37a26682361970500a27206a3bc6638e9feee990
Alalabu <253993420@qq.com>
2020-04-28 18:29:40 +0800

    update ctx.message

--- config/message.config.js
+++ config/message.config.js
@@ -6,9 +6,14 @@
  */
 module.exports = {
   success: [ 0, 'SUCCESS' ],
+  param: {
+    invalid: [ 100, '参数无效!', 'errlog' ],
+    miss: [ 101, '参数缺失!', 'errlog' ],
+  },
   auth: {
     noAuth: [ 1001, '您没有权限访问该接口!', 'errlog' ],
     timeout: [ 1002, 'token 已过期! 请重新获取令牌', 'warnlog' ],
     invalid: [ 1003, 'Token 令牌不合法!', 'infolog' ],
   },
+  exception: [ -1, 'Unknow Exception', 'errlog' ],
 };
